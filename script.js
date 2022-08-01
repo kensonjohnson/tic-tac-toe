9becded8122f4b4d3096e71ca138fad60a1e955a
kensonjohnson <kenson.johnson@outlook.com>
2022-07-31 21:21:03 -0500

    Front End Complete
    Finished up the UI and added classes that can be handled by the
    script. The "x" and "o" are created completely within the CSS and
    applied by adding a .x or .o class to a cell div. Hover affect is
    achieved by adding .x or .o to the board div.
    
    HTML and CSS files are finished.
    Next is the JS script.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-const gameBoard = () => {
-  let board = [];
+//gameBoard is created as a module
+const gameBoard = (() => {
+  let currentBoard = ["x", "o", "x", "o", "x", "o", "x", "o", "x"];
+  const winningCombos = [];
+
+  //Create function to pass currentBoard to displayController
+  const getCurrentBoard = () => {
+    return currentBoard;
+  };
+
+  //Create function to check for a winner or a draw
+})();
+
+//Create displayController module
+
+const displayController = (() => {
+  //create function to render currentBoard
+  const displayBoard = () => {
+    let gameBoard = gameBoard.getCurrentBoard();
+    //loop through and attch divs to show an "x" or an "o"
+  };
+})();
+
+//Create factory function for player objects
+const Player = () => {
+  //create function to take move
+  //create function to switch turn
 };
